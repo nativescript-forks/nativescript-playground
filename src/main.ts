ec340b4e9d6d5cc7b5409a5ba5733030ab39a1d8
roblav96 <roblav96@gmx.com>
2016-10-03 10:20:14 -0400

    ok time for browserify stuffz

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,10 @@ console.time('bootstrapModule')
 global.tnsconsole.log('bootstrapModule')
 import "reflect-metadata"
 import { NgModule, enableProdMode } from "@angular/core"
-import { platformNativeScriptDynamic, NativeScriptModule, AppOptions } from "nativescript-angular/platform"
+import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform"
 import { NativeScriptRouterModule } from "nativescript-angular/router"
-import { NativeScriptFormsModule } from "nativescript-angular/forms"
-import { registerElement } from "nativescript-angular/element-registry"
 import { routes, routeComponents } from "./routes"
 import { AppComponent } from "./routes/app/app.component"
-import { ModalDialogService } from "nativescript-angular/directives/dialogs"
 
 
 
@@ -28,14 +25,12 @@ enableProdMode()
 		NativeScriptModule,
 		NativeScriptRouterModule,
 		NativeScriptRouterModule.forRoot(routes),
-		// NativeScriptFormsModule,
 	],
-	// providers: [ModalDialogService],
 })
+
 class AppModule { }
 
 platformNativeScriptDynamic().bootstrapModule(AppModule)
-
 // .then(function(ngmodule: any) {
 // 	global.tnsconsole.log('bootstrapModule> DONE')
 // })
