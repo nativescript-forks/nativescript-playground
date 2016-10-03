7fed05d54402d123bae02720250eba898d84b260
roblav96 <roblav96@gmx.com>
2016-10-03 10:08:07 -0400

    BEFORE USING TNS NG2 TEMPLATE

---
+++ src/main-module.ts
@@ -0,0 +1,35 @@
+﻿// 
+
+import { Component, NgModule, ApplicationRef } from "@angular/core"
+import { NativeScriptModule } from "nativescript-angular/platform"
+import { NativeScriptRouterModule } from "nativescript-angular/router"
+import { registerElement } from "nativescript-angular/element-registry"
+import { routes, routeComponents } from "./routes"
+import { AppComponent } from "./routes/app/app.component"
+
+
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		...routeComponents,
+	],
+	bootstrap: [
+		AppComponent,
+	],
+	imports: [
+		NativeScriptModule,
+		NativeScriptRouterModule,
+		NativeScriptRouterModule.forRoot(routes),
+	],
+	entryComponents: [
+		AppComponent,
+		...routeComponents,
+	]
+})
+
+export class MainModule {
+	constructor(appRef: ApplicationRef) {
+		appRef.bootstrap(AppComponent)
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,14 @@
 ﻿// 
 
-import "./dev/tns.console"
-console.time('bootstrapModule')
-global.tnsconsole.log('bootstrapModule')
+// import "./dev/tns.console"
+// console.time("bootstrapModule")
+// global.tnsconsole.log("bootstrapModule")
 import "reflect-metadata"
-import { NgModule, enableProdMode } from "@angular/core"
-import { platformNativeScriptDynamic, NativeScriptModule, AppOptions } from "nativescript-angular/platform"
-import { NativeScriptRouterModule } from "nativescript-angular/router"
-import { NativeScriptFormsModule } from "nativescript-angular/forms"
-import { registerElement } from "nativescript-angular/element-registry"
-import { routes, routeComponents } from "./routes"
-import { AppComponent } from "./routes/app/app.component"
-import { ModalDialogService } from "nativescript-angular/directives/dialogs"
+import { enableProdMode } from "@angular/core"
+import { platformNativeScriptDynamic } from "nativescript-angular/platform"
+import { MainModuleNgFactory } from "./main-module.ngfactory"
 
 
 
-enableProdMode()
-@NgModule({
-	declarations: [
-		AppComponent,
-		...routeComponents,
-	],
-	bootstrap: [
-		AppComponent,
-	],
-	imports: [
-		NativeScriptModule,
-		NativeScriptRouterModule,
-		NativeScriptRouterModule.forRoot(routes),
-		// NativeScriptFormsModule,
-	],
-	// providers: [ModalDialogService],
-})
-class AppModule { }
+platformNativeScriptDynamic().bootstrapModuleFactory(MainModuleNgFactory)
 
-platformNativeScriptDynamic().bootstrapModule(AppModule)
-
-// .then(function(ngmodule: any) {
-// 	global.tnsconsole.log('bootstrapModule> DONE')
-// })
