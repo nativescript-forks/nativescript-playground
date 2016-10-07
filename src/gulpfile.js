d90c86f866dd10d525b9427939ba48ce7a8fc7cd
roblav96 <roblav96@gmx.com>
2016-10-07 04:14:30 -0400

    THIS IS NOW BLANK

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,12 +4,13 @@
 var gulp = require('gulp')
 var ts = require('gulp-typescript')
 var merge = require('merge2')
+var del = require('del')
 
 
 
 /*=====  TSC  ======*/
 let tsProject = ts.createProject('../tsconfig.json')
-gulp.task('apply:tsc', function () {
+gulp.task('apply:tsc', () => {
 	let result = gulp.src([
 		'./**/*.ts',
 		'!./node_modules/**/*'
@@ -22,44 +23,83 @@ gulp.task('apply:tsc', function () {
 
 
 
-/*=====  XML  ======*/
-gulp.task('apply:xml', () => {
+/*=====  TEMPLATES  ======*/
+gulp.task('apply:templates', () => {
 	return merge(gulp.src([
 		'./**/*.xml',
+		'./**/*.html',
 		'!./node_modules/**/*'
 	]).pipe(gulp.dest('../app')))
 })
 
-gulp.task('watch:xml', () => {
+gulp.task('watch:templates', () => {
 	return gulp.watch([
 		'./**/*.xml',
+		'./**/*.html',
 		'!./node_modules/**/*'
-	], ['apply:xml'])
+	], ['apply:templates'])
 })
 
 
 
-/*=====  CSS  ======*/
-gulp.task('apply:css', () => {
+/*=====  STYLES  ======*/
+gulp.task('apply:styles', () => {
 	return merge(gulp.src([
 		'./**/*.css',
 		'!./node_modules/**/*'
 	]).pipe(gulp.dest('../app')))
 })
 
-gulp.task('watch:css', () => {
+gulp.task('watch:styles', () => {
 	return gulp.watch([
 		'./**/*.css',
 		'!./node_modules/**/*'
-	], ['apply:css'])
+	], ['apply:styles'])
 })
 
 
 
-gulp.task('default', ['apply:tsc', 'apply:css', 'apply:xml', 'watch:css', 'watch:xml'], function () {
+/*=====  FONTS  ======*/
+gulp.task('apply:fonts', () => {
+	return merge(gulp.src([
+		'./**/*.ttf',
+		'!./node_modules/**/*'
+	]).pipe(gulp.dest('../app')))
+})
+
+
+
+/*=====  CLEAN JS  ======*/
+gulp.task('clean:js', () => {
+	return del([
+		'./**/*.js',
+		'!./node_modules/**/*',
+		'!./gulpfile.js',
+	])
+})
+
+
+
+/*=====  CLEAN APP  ======*/
+gulp.task('clean:app', () => {
+	return del([
+		'../app/**/*',
+		'!../app/App_Resources',
+		'!../app/App_Resources/**/*',
+		'!../app/package.json',
+	], {
+		force: true,
+	})
+})
+
+
+
+gulp.task('default', ['clean:js', 'apply:fonts', 'apply:styles', 'apply:templates', 'apply:tsc', 'watch:styles', 'watch:templates'], function () {
 	gulp.watch([
 		'./**/*.ts',
-		'!./node_modules/**/*'
+		'!./node_modules/**/*',
 	], ['apply:tsc'])
 })
+
+gulp.task('clean', ['clean:app', 'clean:js'])
 
