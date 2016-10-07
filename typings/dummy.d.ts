a1a020d78190804fe6a563b22798d08b3df6f9ab
roblav96 <roblav96@gmx.com>
2016-10-07 03:43:06 -0400

    added typings@package.json

--- typings/dummy.d.ts
+++ typings/dummy.d.ts
@@ -4,11 +4,6 @@ declare interface ArrayConstructor {
     create(arg1: any, arg2: any): any
 }
 
-// declare module com { }
-// declare var com: any
-// declare var process: any
-declare var Interfaces: any
-
 
 
 declare module "url" {
@@ -30,25 +25,6 @@ declare module "url" {
     export function parse(urlStr: string, parseQueryString?: boolean, slashesDenoteHost?: boolean): Url;
     export function format(url: Url): string;
     export function resolve(from: string, to: string): string;
-}
-
-declare module "events" {
-    export class EventEmitter {
-        static EventEmitter: EventEmitter;
-        static listenerCount(emitter: EventEmitter, event: string): number; // deprecated
-        static defaultMaxListeners: number;
-
-        addListener(event: string, listener: Function): this;
-        on(event: string, listener: Function): this;
-        once(event: string, listener: Function): this;
-        removeListener(event: string, listener: Function): this;
-        removeAllListeners(event?: string): this;
-        setMaxListeners(n: number): this;
-        getMaxListeners(): number;
-        listeners(event: string): Function[];
-        emit(event: string, ...args: any[]): boolean;
-        listenerCount(type: string): number;
-    }
 }
 
 
