af76e15d741f86de03acf9ddb05693fe01127679
roblav96 <roblav96@gmx.com>
2016-11-15 08:17:08 -0500

    da imiimaege

---
+++ src/iimage/iimage.ios.ts
@@ -0,0 +1,11 @@
+// 
+
+import * as application from 'application'
+import { View } from 'ui/core/view'
+
+
+
+export class IImage extends View {
+	
+}
+
---
+++ src/iimage/iimage.ts
@@ -0,0 +1,11 @@
+// 
+
+import * as application from 'application'
+import { View } from 'ui/core/view'
+
+
+
+export declare class IImage extends View {
+	
+}
+
